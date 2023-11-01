6ab35da74c6b2e4ca2262bf088559575979f0d1d
Wojciech Pasternak <wojciech.pasternakk@gmail.com>
2023-11-01 17:33:53 +0100

    Show options after click button

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
 const bookmark = document.querySelector(".bookmark");
+const optionsBtn = document.querySelectorAll(".options__info .btn");
 
+// Function select bookmark button
 function changeBtnColor() {
   this.classList.toggle("active");
   if (this.classList.contains("active")) {
@@ -9,4 +11,21 @@ function changeBtnColor() {
   }
 }
 
+// Function hide all options
+function hideAllOptions() {
+  optionsBtn.forEach((option) => {
+    option.parentElement.nextElementSibling.style.display = "none";
+  });
+}
+
+//Function to show available option
+function showOptionsElement() {
+  hideAllOptions();
+  this.parentElement.nextElementSibling.style.display = "flex";
+}
+
 bookmark.addEventListener("click", changeBtnColor);
+
+optionsBtn.forEach((option) => {
+  option.addEventListener("click", showOptionsElement);
+});
