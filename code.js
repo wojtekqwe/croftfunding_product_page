9a5605e17cf5a93377a4f4f5fdc638fd80c62991
Wojciech Pasternak <wojciech.pasternakk@gmail.com>
2023-11-04 17:49:44 +0100

    Change operating principle, add new function

--- code.js
+++ code.js
@@ -1,42 +1,129 @@
-const bookmark = document.querySelector(".bookmark");
 const optionsBtn = document.querySelectorAll(".options__info .btn");
-const optionsBtnModal = document.querySelectorAll(".options__option--modal");
 
-// Function select bookmark button
-function changeBtnColor() {
-  this.classList.toggle("active");
-  if (this.classList.contains("active")) {
-    this.querySelector("p").textContent = "Bookmarked";
-  } else {
-    this.querySelector("p").textContent = "Bookmark";
-  }
-}
+let payContainer = document.querySelector("#collect-money");
+let backerContainer = document.querySelector("#total-backers");
 
 // Function hide all options
-function hideAllOptions() {
-  optionsBtn.forEach((option) => {
-    option.parentElement.parentElement.classList.remove("active");
+function hideAllOptions(container) {
+  const parentElement = container.parentElement;
+  const activeElement = parentElement.querySelectorAll(".active");
+
+  activeElement.forEach((el) => {
+    el.classList.remove("active");
   });
 }
 
-bookmark.addEventListener("click", changeBtnColor);
+// Update data in stats and product section
+function updateData() {}
+
+// Show last modal
+function showSummary() {
+  updateData();
+}
+
+function checkOption(element) {
+  const findElement = element.target.closest(".options__option");
+  hideAllOptions(findElement);
+  findElement.classList.add("active");
+}
 
 // Option in main view
 optionsBtn.forEach((option) => {
-  option.addEventListener("click", function () {
-    hideAllOptions();
-    const container = this.parentElement.parentElement;
+  option.addEventListener("click", checkOption);
+  // option.addEventListener("click", function () {
+  //   hideAllOptions();
+  //   const container = this.parentElement.parentElement;
+  //   container.classList.toggle("active");
 
-    container.classList.toggle("active");
-  });
+  //   const btnContinue = container.querySelector(".options__pays .btn");
+  //   const input = container.querySelector("#ammount");
+  //   const productLeft = document.querySelector(".left");
+  //   btnContinue.addEventListener("click", () => {
+  //     showSummary();
+  //     payContainer.textContent =
+  //       Number(payContainer.textContent) + Number(input.value);
+  //     backerContainer.textContent = Number(backerContainer.textContent) + 1;
+  //     productLeft.textContent = Number(productLeft.textContent) - 1;
+
+  //     if (Number(productLeft.textContent <= 0)) {
+  //       option.classList.add("options__option--finish");
+  //     }
+  //   });
+  // });
 });
 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// const optionsBtnModal = document.querySelectorAll(".options__option--modal");
 // Option in modal
-optionsBtnModal.forEach((option) => {
-  option.addEventListener("click", function () {
-    optionsBtnModal.forEach((option) => {
-      option.classList.remove("active");
-    });
-    this.classList.toggle("active");
-  });
-});
+// optionsBtnModal.forEach((option) => {
+//   option.addEventListener("click", function () {
+//     optionsBtnModal.forEach((option) => {
+//       option.classList.remove("active");
+//     });
+//     this.classList.toggle("active");
+//   });
+// });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// const bookmark = document.querySelector(".bookmark");
+// Function select bookmark button
+// function changeBtnColor() {
+//   this.classList.toggle("active");
+//   if (this.classList.contains("active")) {
+//     this.querySelector("p").textContent = "Bookmarked";
+//   } else {
+//     this.querySelector("p").textContent = "Bookmark";
+//   }
+// }
+// Reaction for click icon bookmarks
+// bookmark.addEventListener("click", changeBtnColor);
